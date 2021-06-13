c3b21cc0e40a5e9a6701e56f1cc09260fa20d426
Karaskiv <82354360+Karaskiv@users.noreply.github.com>
2021-06-14 00:33:12 +0700

    MainActivity done, 1 error

--- app/src/main/java/space/karaskiv/prep1/MainActivity.java
+++ app/src/main/java/space/karaskiv/prep1/MainActivity.java
@@ -3,12 +3,134 @@ package space.karaskiv.prep1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import space.karaskiv.prep1.pojo.MultipleResource;
+import space.karaskiv.prep1.pojo.User;
+import space.karaskiv.prep1.pojo.UserList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView responseText;
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        responseText = (TextView)findViewById(R.id.responseText);
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+
+        /**
+         GET List Resources
+         **/
+        Call<MultipleResource> call = apiInterface.doGetListResources();
+        call.enqueue(new Callback<MultipleResource>() {
+            @Override
+            public void onResponse(Call<MultipleResource> call, Response<MultipleResource> response) {
+
+                Log.d("TAG",response.code()+"");
+
+                String displayResponse = "";
+
+                MultipleResource resource = response.body();
+                Integer text = resource.page;
+                Integer total = resource.total;
+                Integer totalPages = resource.totalPages;
+                List<MultipleResource.Datum> datumList = resource.data;
+                displayResponse += text + " Page\n";
+
+                for (MultipleResource.Datum datum : datumList) {
+                    displayResponse += datum.id + " " + "\n";
+                }
+
+                responseText.setText(displayResponse);
+
+            }
+
+            @Override
+            public void onFailure(Call<MultipleResource> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        /**
+         Create New User
+         **/
+        User user = new User("karaskiv", "leader");
+        Call<User> call1 = apiInterface.createUser(user);
+        call1.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user1 = response.body();
+
+                Toast.makeText(getApplicationContext(), user1.name + " " + user1.job + " " + user1.id + " " + user1.createdAt, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        /**
+         GET List Users
+         **/
+        Call<UserList> call2 = apiInterface.doGetListResources("2");
+        call2.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.total;
+                List<UserList.Datum> datumList = userList.data;
+                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
+
+                for (UserList.Datum datum : datumList) {
+                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+            }
+        });
+
+        /**
+         POST name and job Url encoded
+         **/
+        Call<UserList> call3 = apiInterface.doCreateUserWithField("karaskiv", "leader");
+        call3.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
+
+                for (UserList.Datum datum : datumList) {
+                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+            }
+        });
     }
 }
